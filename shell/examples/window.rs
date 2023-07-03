896afea262e48b2fa26b959ffb74004e5cf09ca5
Armin Sander <armin@replicator.org>
2023-07-03 08:08:13 +0200

    shell: Render a triangle

--- shell/examples/window.rs
+++ shell/examples/window.rs
@@ -1,5 +1,104 @@
+use anyhow::Result;
+
 #[tokio::main]
 async fn main() {
     env_logger::init();
-    granularity_shell::run().await;
+
+    granularity_shell::run(render).await;
+}
+
+fn render(
+    device: &wgpu::Device,
+    surface: &wgpu::Surface,
+    config: &wgpu::SurfaceConfiguration,
+) -> Result<(wgpu::CommandBuffer, wgpu::SurfaceTexture)> {
+    // TODO: Retrieve the surface configuration from the surface.
+
+    let shader = device.create_shader_module(wgpu::include_wgsl!("shader.wgsl"));
+
+    let render_pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+        label: Some("Render Pipeline Layout"),
+        bind_group_layouts: &[],
+        push_constant_ranges: &[],
+    });
+
+    let targets = &[Some(wgpu::ColorTargetState {
+        format: config.format,
+        blend: Some(wgpu::BlendState::REPLACE),
+        write_mask: wgpu::ColorWrites::ALL,
+    })];
+
+    let pipeline = wgpu::RenderPipelineDescriptor {
+        label: Some("Render Pipeline"),
+        layout: Some(&render_pipeline_layout),
+        vertex: wgpu::VertexState {
+            module: &shader,
+            entry_point: "vs_main", // 1.
+            buffers: &[],           // 2.
+        },
+        fragment: Some(wgpu::FragmentState {
+            // 3.
+            module: &shader,
+            entry_point: "fs_main",
+            targets,
+        }),
+        primitive: wgpu::PrimitiveState {
+            topology: wgpu::PrimitiveTopology::TriangleList, // 1.
+            strip_index_format: None,
+            front_face: wgpu::FrontFace::Ccw, // 2.
+            cull_mode: Some(wgpu::Face::Back),
+            // Setting this to anything other than Fill requires Features::NON_FILL_POLYGON_MODE
+            polygon_mode: wgpu::PolygonMode::Fill,
+            // Requires Features::DEPTH_CLIP_CONTROL
+            unclipped_depth: false,
+            // Requires Features::CONSERVATIVE_RASTERIZATION
+            conservative: false,
+        },
+        depth_stencil: None, // 1.
+        multisample: wgpu::MultisampleState {
+            count: 1,                         // 2.
+            mask: !0,                         // 3.
+            alpha_to_coverage_enabled: false, // 4.
+        },
+        multiview: None, // 5.
+    };
+
+    let pipeline = device.create_render_pipeline(&pipeline);
+
+    let mut encoder = device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
+        label: Some("Render Encoder"),
+    });
+
+    let output = surface.get_current_texture()?;
+    let view = output
+        .texture
+        .create_view(&wgpu::TextureViewDescriptor::default());
+
+    {
+        let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+            label: Some("Render Pass"),
+            color_attachments: &[
+                // This is what @location(0) in the fragment shader targets
+                Some(wgpu::RenderPassColorAttachment {
+                    view: &view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(wgpu::Color {
+                            r: 0.1,
+                            g: 0.2,
+                            b: 0.3,
+                            a: 1.0,
+                        }),
+                        store: true,
+                    },
+                }),
+            ],
+            depth_stencil_attachment: None,
+        });
+
+        render_pass.set_pipeline(&pipeline);
+        render_pass.draw(0..3, 0..1);
+    }
+
+    Ok((encoder.finish(), output))
 }
--- shell/src/lib.rs
+++ shell/src/lib.rs
@@ -1,3 +1,4 @@
+use anyhow::Result;
 use log::{error, info};
 use wgpu::PresentMode;
 use winit::{
@@ -119,44 +120,63 @@ impl State {
 
     fn update(&mut self) {}
 
-    fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
-        let output = self.surface.get_current_texture()?;
-        let view = output
-            .texture
-            .create_view(&wgpu::TextureViewDescriptor::default());
-        let mut encoder = self
-            .device
-            .create_command_encoder(&wgpu::CommandEncoderDescriptor {
-                label: Some("Render Encoder"),
-            });
+    fn render(
+        &mut self,
+        f: impl Fn(
+            &wgpu::Device,
+            &wgpu::Surface,
+            &wgpu::SurfaceConfiguration,
+        ) -> Result<(wgpu::CommandBuffer, wgpu::SurfaceTexture)>,
+    ) -> Result<(), wgpu::SurfaceError> {
+        let (command_buffer, surface_texture) =
+            f(&self.device, &self.surface, &self.config).expect("render failed");
 
-        {
-            let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-                label: Some("Render Pass"),
-                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
-                    view: &view,
-                    resolve_target: None,
-                    ops: wgpu::Operations {
-                        load: wgpu::LoadOp::Clear(wgpu::Color {
-                            r: 0.1,
-                            g: 0.2,
-                            b: 0.3,
-                            a: 1.0,
-                        }),
-                        store: true,
-                    },
-                })],
-                depth_stencil_attachment: None,
-            });
-        }
-        self.queue.submit([encoder.finish()]);
-        output.present();
+        // let view = output
+        //     .texture
+        //     .create_view(&wgpu::TextureViewDescriptor::default());
+        // let mut encoder = self
+        //     .device
+        //     .create_command_encoder(&wgpu::CommandEncoderDescriptor {
+        //         label: Some("Render Encoder"),
+        //     });
+
+        // {
+        //     let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+        //         label: Some("Render Pass"),
+        //         color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+        //             view: &view,
+        //             resolve_target: None,
+        //             ops: wgpu::Operations {
+        //                 load: wgpu::LoadOp::Clear(wgpu::Color {
+        //                     r: 0.1,
+        //                     g: 0.2,
+        //                     b: 0.3,
+        //                     a: 1.0,
+        //                 }),
+        //                 store: true,
+        //             },
+        //         })],
+        //         depth_stencil_attachment: None,
+        //     });
+        // }
+
+        // self.queue.submit([encoder.finish()]);
+
+        self.queue.submit([command_buffer]);
+        surface_texture.present();
 
         Ok(())
     }
 }
 
-pub async fn run() {
+pub async fn run(
+    f: impl Fn(
+            &wgpu::Device,
+            &wgpu::Surface,
+            &wgpu::SurfaceConfiguration,
+        ) -> Result<(wgpu::CommandBuffer, wgpu::SurfaceTexture)>
+        + 'static,
+) {
     let event_loop = EventLoop::new();
     let window = WindowBuilder::new().build(&event_loop).unwrap();
     let mut state = State::new(&window).await;
@@ -191,7 +211,7 @@ pub async fn run() {
 
             Event::RedrawRequested(window_id) if window_id == window.id() => {
                 state.update();
-                match state.render() {
+                match state.render(&f) {
                     Ok(_) => {}
                     // Reconfigure the surface if lost
                     // TODO: shouldn't we redraw here?
