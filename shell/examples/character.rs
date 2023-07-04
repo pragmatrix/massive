09829cbe22179e3bb073575b9282df9546f2d1fe
Armin Sander <armin@replicator.org>
2023-07-04 09:18:38 +0200

    style: black on white

--- shell/examples/character.rs
+++ shell/examples/character.rs
@@ -140,7 +140,7 @@ fn render(
                 view: &view,
                 resolve_target: None,
                 ops: wgpu::Operations {
-                    load: wgpu::LoadOp::Load,
+                    load: wgpu::LoadOp::Clear(wgpu::Color::WHITE),
                     store: true,
                 },
             })],
