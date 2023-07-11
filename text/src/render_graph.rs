7945bc2b1b611870b5dcdb32362de59bf3159f59
Armin Sander <armin@replicator.org>
2023-07-11 08:43:54 +0200

    Use alpha blending

--- text/src/render_graph.rs
+++ text/src/render_graph.rs
@@ -266,7 +266,7 @@ pub fn render_graph(
 
     let targets = map_ref!(|config| [Some(wgpu::ColorTargetState {
         format: config.format,
-        blend: Some(wgpu::BlendState::REPLACE),
+        blend: Some(wgpu::BlendState::ALPHA_BLENDING),
         write_mask: wgpu::ColorWrites::ALL,
     })]);
 
