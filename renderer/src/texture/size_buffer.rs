58657d017f4f24a74f8b8e8262b93eb65c42716d
Armin Sander <armin@replicator.org>
2023-08-15 09:47:37 +0200

    Fix texture size

--- renderer/src/texture/size_buffer.rs
+++ renderer/src/texture/size_buffer.rs
@@ -1,13 +1,17 @@
 use wgpu::util::DeviceExt;
 
+use crate::pods;
+
 #[derive(Debug)]
 pub struct SizeBuffer(wgpu::Buffer);
 
 impl SizeBuffer {
     pub fn new(device: &wgpu::Device, size: (u32, u32)) -> Self {
+        let uniform = pods::TextureSize([size.0 as f32, size.1 as f32]);
+
         let buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
             label: Some("Texture Size Buffer"),
-            contents: bytemuck::cast_slice(&[size.0, size.1]),
+            contents: bytemuck::cast_slice(&[uniform]),
             usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
         });
 
