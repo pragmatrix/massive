4a4b5d7fbbfd4f4211ba10434f28cc0dd60f796f
Armin Sander <armin@replicator.org>
2024-03-12 19:51:52 +0100

    Replace nearly by approx

--- renderer/src/glyph/glyph_classifier.rs
+++ renderer/src/glyph/glyph_classifier.rs
@@ -1,5 +1,5 @@
+use approx::{abs_diff_eq, ulps_eq};
 use granularity_geometry::Point3;
-use nearly::nearly_eq;
 
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub enum GlyphClass {
@@ -21,8 +21,8 @@ pub enum DistortionClass {
 
 /// For planar comparisons of Z values (we might transform them to pixels too, this way we can use
 /// PIXEL_EPSILON).
-const ULPS_Z: i64 = 8;
-/// One thousandth of a pixel should be good enough.
+const ULPS_Z: u32 = 8;
+/// A ten-thousandth of a pixel should be good enough.
 const PIXEL_EPSILON: f64 = 0.0001;
 
 impl GlyphClass {
@@ -37,17 +37,17 @@ impl GlyphClass {
         // TODO: 3 Points might be enough.
 
         // TODO: may compare z for quad[3]?
-        let planar_z = nearly_eq!(quad[0].z, quad[1].z, ulps = ULPS_Z)
-            && nearly_eq!(quad[0].z, quad[2].z, ulps = ULPS_Z);
+        let planar_z = ulps_eq!(quad[0].z, quad[1].z, max_ulps = ULPS_Z)
+            && ulps_eq!(quad[0].z, quad[2].z, max_ulps = ULPS_Z);
 
         if !planar_z {
             return GlyphClass::Distorted(DistortionClass::NonPlanar);
         }
 
-        let rectangular = nearly_eq!(quad[0].y, quad[1].y, eps = PIXEL_EPSILON)
-            && nearly_eq!(quad[2].y, quad[3].y, eps = PIXEL_EPSILON)
-            && nearly_eq!(quad[0].x, quad[3].x, eps = PIXEL_EPSILON)
-            && nearly_eq!(quad[1].x, quad[2].x, eps = PIXEL_EPSILON);
+        let rectangular = pixel_eq(quad[0].y, quad[1].y)
+            && pixel_eq(quad[2].y, quad[3].y)
+            && pixel_eq(quad[0].x, quad[3].x)
+            && pixel_eq(quad[1].x, quad[2].x);
 
         if !rectangular {
             return GlyphClass::Distorted(DistortionClass::NonRectangular);
@@ -57,21 +57,25 @@ impl GlyphClass {
         let scale_x = quad[1].x - quad[0].x;
         let scale_y = quad[2].y - quad[0].y;
 
-        let quadratic = nearly_eq!(scale_x, scale_y, eps = PIXEL_EPSILON);
+        let quadratic = pixel_eq(scale_x, scale_y);
         if !quadratic {
             return GlyphClass::Distorted(DistortionClass::NonQuadratic);
         }
 
-        let pixel_perfect = nearly_eq!(scale_x, 1.0, eps = PIXEL_EPSILON);
+        let pixel_perfect = pixel_eq(scale_x, 1.0);
         if !pixel_perfect {
             return GlyphClass::Zoomed((scale_x + scale_y) / 2.0);
         }
 
-        let aligned_x = nearly_eq!(quad[0].x, quad[0].x.floor(), eps = PIXEL_EPSILON);
-        let aligned_y = nearly_eq!(quad[0].y, quad[0].y.floor(), eps = PIXEL_EPSILON);
+        let aligned_x = pixel_eq(quad[0].x, quad[0].x.floor());
+        let aligned_y = pixel_eq(quad[0].y, quad[0].y.floor());
 
         GlyphClass::PixelPerfect {
             alignment: (aligned_x, aligned_y),
         }
     }
+}
+
+fn pixel_eq(a: f64, b: f64) -> bool {
+    abs_diff_eq!(a, b, epsilon = PIXEL_EPSILON)
 }
