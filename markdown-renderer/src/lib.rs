daf32b72b3eaf0a32a35352e6724a8af38fda470
Armin Sander <armin@replicator.org>
2024-03-20 14:36:56 +0100

    Add glphon (because of cosmic text), inlyne, converting it to a lib, remove granularity, add the markdown renderer project (yet empty)

---
+++ markdown-renderer/src/lib.rs
@@ -0,0 +1,21 @@
+use inlyne::interpreter::HtmlInterpreter;
+
+
+
+
+
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
