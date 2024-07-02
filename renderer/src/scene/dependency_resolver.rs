7da263cfde430e530595b320147998f77f7a3886
Armin Sander <armin@replicator.org>
2024-07-02 10:25:42 +0200

    caches -> computed_storage

--- renderer/src/scene/dependency_resolver.rs
+++ renderer/src/scene/dependency_resolver.rs
@@ -6,14 +6,14 @@ use massive_scene::Id;
 /// Invoking this function ensures that the computed value `id` is up to date with its dependencies
 /// at `head_version`.
 ///
-/// We don't return a reference to the result here, because the borrow checker would make this
-/// recursive function invocation unnecessarily more complex.
+/// We don't return a reference to the computed value, because the borrow checker would not be
+/// able to unborrow `computed_storage` after a return (a current limitation).
 ///
 /// TODO: Unrecurse this. There might be degenerate cases of large dependency chains.
 pub fn resolve<Resolver: DependencyResolver>(
     head_version: Version,
     shared_storage: &Resolver::SharedStorage,
-    caches: &mut Resolver::ComputedStorage,
+    computed_storage: &mut Resolver::ComputedStorage,
     id: Id,
 ) where
     Computed<Resolver::Computed>: Default,
@@ -22,30 +22,30 @@ pub fn resolve<Resolver: DependencyResolver>(
     //
     // `get_or_default` must be used here. This is the only situation in which the cache may
     // need to be resized.
-    let computed = Resolver::computed_mut(caches, id);
-    if computed.validated_at == head_version {
+    if Resolver::computed_mut(computed_storage, id).validated_at == head_version {
         return;
     }
+
     // Save the current max dependencies version for later.
     //
     // In theory this could be overwritten if there are cycles in the dependency graph, but in
     // practice they are not (and everything may blow up anyway).
-    let computed_max_deps = computed.max_deps_version;
+    let computed_max_deps = Resolver::computed_mut(computed_storage, id).max_deps_version;
 
     let source = Resolver::source(shared_storage, id);
     let max_deps_version =
-        Resolver::resolve_dependencies(head_version, source, shared_storage, caches);
+        Resolver::resolve_dependencies(head_version, source, shared_storage, computed_storage);
 
     // If the max_deps_version is smaller or equal to the one of the computed value, the value is ok
     // and can be marked as validated at `head_version`.
     if max_deps_version <= computed_max_deps {
-        Resolver::computed_mut(caches, id).validated_at = head_version;
+        Resolver::computed_mut(computed_storage, id).validated_at = head_version;
         return;
     }
 
     // Compute a new value and store it.
-    let new_value = Resolver::compute(shared_storage, caches, source);
-    *Resolver::computed_mut(caches, id) = Computed {
+    let new_value = Resolver::compute(shared_storage, computed_storage, source);
+    *Resolver::computed_mut(computed_storage, id) = Computed {
         validated_at: head_version,
         max_deps_version,
         value: new_value,
