dc275915f60efe3d9842693ab6bea35f97725426
Armin Sander <armin@replicator.org>
2024-08-12 09:52:35 +0200

    animation -> blended_animation

--- animation/src/lib.rs
+++ animation/src/lib.rs
@@ -1,10 +1,10 @@
-mod animation;
+mod blended_animation;
 mod interpolatable;
 mod interpolation;
 mod tickery;
 mod timeline;
 
-pub use animation::*;
+pub use blended_animation::*;
 pub use interpolatable::*;
 pub use interpolation::*;
 pub use tickery::*;
