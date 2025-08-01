b0f40c785c15c8041c3b374488c49d4a63b44311
Armin Sander <armin@replicator.org>
2025-08-01 07:04:26 +0200

    Comment

--- scene/src/id.rs
+++ scene/src/id.rs
@@ -2,6 +2,9 @@ use derive_more::Deref;
 
 /// An identifier that can be used to index into rows to allow fast id associative storage and
 /// retrieval of objects.
+///
+/// Ids start at 0 and their maximum value are never greater than the number of ids acquired. This
+/// makes them optimal for using them as storage indices.
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, Deref)]
 pub struct Id(usize);
 
