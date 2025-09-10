0cd74b27020872c9701031cba94d2fd19c327e83
Armin Sander <armin@replicator.org>
2025-09-10 12:40:40 +0200

    Remove unused functions (which are confusing)

--- input/src/sensor.rs
+++ input/src/sensor.rs
@@ -11,16 +11,4 @@ impl ButtonSensor {
     pub fn new(device: DeviceId, button: MouseButton) -> Self {
         Self { device, button }
     }
-
-    pub fn left(&self) -> Option<ButtonSensor> {
-        (self.button == MouseButton::Left).then_some(*self)
-    }
-
-    pub fn middle(&self) -> Option<ButtonSensor> {
-        (self.button == MouseButton::Middle).then_some(*self)
-    }
-
-    pub fn right(&self) -> Option<ButtonSensor> {
-        (self.button == MouseButton::Right).then_some(*self)
-    }
 }
