5ccc5412318d405dee165088f49302e07984ed8d
Armin Sander <armin@replicator.org>
2025-12-18 12:34:54 +0100

    Detail

--- desktop/src/focus_manager.rs
+++ desktop/src/focus_manager.rs
@@ -99,6 +99,7 @@ impl FocusManager {
 }
 
 impl FocusedInstance {
+    #[must_use]
     pub fn focus_view(&mut self, new_view: Option<ViewId>) -> Vec<FocusTransition> {
         if self.focused_view == new_view {
             return Vec::new();
@@ -112,6 +113,7 @@ impl FocusedInstance {
         transitions
     }
 
+    #[must_use]
     pub fn unfocus_view(&mut self) -> Vec<FocusTransition> {
         self.focused_view
             .take()
