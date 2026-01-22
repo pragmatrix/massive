0e46399541746d59c029e7d808a982e590f17ea7
Armin Sander <armin@replicator.org>
2026-01-22 11:35:40 +0100

    Remove the NodeLayouter, it's not used anymore

--- layout/src/lib.rs
+++ layout/src/lib.rs
@@ -3,10 +3,8 @@ use derive_more::{Deref, From, Into};
 mod dimensional;
 mod dimensional_types;
 mod layouter;
-mod node_layouter;
 
 pub use layouter::{BoxComponents as Box, Layouter};
-pub use node_layouter::{LayoutInfo, LayoutNode, layout};
 
 #[derive(Debug, Copy, Clone, From, Into, Deref, Default)]
 pub struct LayoutAxis(usize);
