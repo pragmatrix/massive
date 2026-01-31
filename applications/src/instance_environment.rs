3c0db412a604841304f80faeff6ac35f049e012d
Armin Sander <armin@replicator.org>
2026-01-31 07:39:56 +0100

    Add parameters to the InstanceEnvironment

--- applications/src/instance_environment.rs
+++ applications/src/instance_environment.rs
@@ -1,4 +1,5 @@
 use massive_renderer::FontManager;
+use serde_json::{Map, Value};
 use tokio::sync::mpsc::UnboundedSender;
 
 use crate::{InstanceCommand, InstanceId};
@@ -9,6 +10,7 @@ pub struct InstanceEnvironment {
     // Robustness: This might change on runtime.
     pub(crate) primary_monitor_scale_factor: f64,
     pub(crate) font_manager: FontManager,
+    pub(crate) parameters: Map<String, Value>,
 }
 
 impl InstanceEnvironment {
@@ -21,6 +23,12 @@ impl InstanceEnvironment {
             command_sender: requests_tx,
             primary_monitor_scale_factor,
             font_manager,
+            parameters: Default::default(),
         }
+    }
+
+    pub fn with_parameters(mut self, parameters: Map<String, Value>) -> Self {
+        self.parameters = parameters;
+        self
     }
 }
