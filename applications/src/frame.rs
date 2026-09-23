84ff7c25a4b2625a001d40800d39638cbf5a87cf
Armin Sander <armin@replicator.org>
2026-09-23 11:01:32 +0200

    applications: revert the Default impl for Frame

--- applications/src/frame.rs
+++ applications/src/frame.rs
@@ -124,16 +124,6 @@ where
     }
 }
 
-impl<C> Default for Frame<C>
-where
-    C: From<SceneChange> + std::fmt::Debug + Send + Any + 'static,
-{
-    #[track_caller]
-    fn default() -> Self {
-        Self::new()
-    }
-}
-
 impl<C> AnimationAllocator for Frame<C>
 where
     C: From<SceneChange> + std::fmt::Debug + Send + Any,
